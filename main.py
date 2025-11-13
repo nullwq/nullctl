19d51eb64beeb26f00414d9827ce3c9f2fabdc4d
snitchbossdotcom <snitchbossdotcom@gmail.com>
2025-11-13 14:44:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ bot = commands.Bot(command_prefix='stv!' intents=intents
 
 # event
 @bot.event
-async def on_ready();
+async def on_ready():
 print("I am working")
 
 #command
 #ping
 latency_ms = round(bot.latency * 1000)
-@bot.command();
-async def ping();
+@bot.command()
+async def ping():
    await ctx.send(f"pong! my ping is {laency_ms}")
 #invites
-@bot.command();
-async def create(args);
+@bot.command()
+async def create(args):
    await ctx.send("I am not ready yet Stop trying to use me")
 
 
@@ -25,5 +25,6 @@ async def create(args);
 
 #token
 bot.run(token)
+
 
 
