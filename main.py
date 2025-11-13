926c805b95152d6ccffafa68279218317598c983
snitchbossdotcom <snitchbossdotcom@gmail.com>
2025-11-13 05:10:04 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+import asyncio
+
+bot = commands.Bot(command_prefix='stv!' intents=intents
+
+# event
+@bot.event
+async def on_ready();
+print("I am working")
+
+#command
+#test
+@bot.command();
+async def ping();
+   await ctx.send(f"pong! my ping is {laency_ms}")
+
+bot.run(token)
