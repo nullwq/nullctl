cf54d7a5bfc1e0a67daea5dadad6d667ab15394f
snitchbossdotcom <snitchbossdotcom@gmail.com>
2025-11-13 14:57:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import asyncio
+from typing import Optional
 
 bot = commands.Bot(command_prefix='stv!' intents=intents
 
@@ -19,12 +20,37 @@ async def ping():
 @bot.command()
 async def create(args):
    await ctx.send("I am not ready yet Stop trying to use me")
+#multiply
+@bot.command()
+async def multiply(ctx, num1: float, num2: float):
+   result = num1 * num2
+   await ctx.send(f"The answer of {num1} multiplied by {num2} is {result}")
 
+#addition
+@bot.command()
+async def add(ctx, num1: float, num2: float):
+   result = num1 + num2
+   await ctx.send(f"The answer of {num1} + {num2} is {result}")
+
+#Subtract
+@bot.command()
+async def Subtract(ctx, num1: float, num2: float):
+   result = num1 - num2
+   await ctx.send(f"The answer of {num1} Subtracted by {num2} is {result}")
+
+#divided
+@bot.command()
+async def divide(ctx, num1: float, num2: float):
+   result = num1 / num2
+   await ctx.send(f"The answer of {num1} divided by {num2} is {result}")
+   
+   
 
 
 
 #token
 bot.run(token)
+
 
 
 
