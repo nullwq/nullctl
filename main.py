5c96f543b63c746cee226b81b5115ff6ab7afb0e
snitchbossdotcom <snitchbossdotcom@gmail.com>
2025-11-13 14:43:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ async def on_ready();
 print("I am working")
 
 #command
-#test
+#ping
+latency_ms = round(bot.latency * 1000)
 @bot.command();
-async def ping();
+async def ping()
    await ctx.send(f"pong! my ping is {laency_ms}")
+#invites
+@bot.command();
+async def create(args)
+   await ctx.send("I am not ready yet Stop trying to use me")
 
+
+
+
+#token
 bot.run(token)
+
